47520412786d1764be8ad69a6302672f7c035ae4
Your Name <you@example.com>
2018-05-16 15:06:13 +0400

    dass 5 tnajin

--- lesson5/script.js
+++ lesson5/script.js
@@ -1,24 +1,29 @@
-function main() {
-   var socket = io.connect('http://localhost:8080');
-   var chatDiv = document.getElementById('chat');
-   var input = document.getElementById('message');
-   var button = document.getElementById('submit');
+var r,g,b;
 
-   function handleSubmit(evt) {
-       var val = input.value;
-       if (val != "") {
-           socket.emit("send message", val);
-       }
-   }
-   button.onclick = handleSubmit;
-   function handleMessage(msg) {
-   		var p = document.createElement('p');
-   		p.innerText = msg;
-   		chatDiv.appendChild(p);
-   		input.value = "";
+function setup() {
+    frameRate(5);
+    createCanvas(500, 500);
+    background('#acacac');
+    r = random(255);
+    g = random(255);
+    b = random(255);
 }
 
-socket.on('display message', handleMessage);
+function mouseDragged() {
+  console.log(mouseX, mouseY);
+ 
+  socket.emit("kordinat",[mouseX, mouseY,r,g,b]);
+}
+
+function main() {
+    socket = io.connect('http://localhost:8080');
+    socket.on("gciq",elips);
+
+    
+    function elips (data){
+        fill(data[2],data[3],data[4]);
+        ellipse(data[0],data[1],30,30);
+    }
 } // main closing bracket
 
 window.onload = main;
--- lesson5/server.js
+++ lesson5/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var messages = [];
+var kordinatner = [];
 
 app.use(express.static("."));
 app.get('/', function (req, res) {
@@ -13,16 +13,12 @@ server.listen(8080);
 
 
 io.on('connection', function (socket) {
-   for(var i in messages) {
-     io.sockets.emit("display message", messages[i]);
+   for(var i in kordinatner) {
+     io.sockets.emit("gciq", kordinatner[i]);
    }
-   socket.on("send message", function (data) {
-       messages.push(data);
-       io.sockets.emit("display message", data);
+   socket.on("kordinat", function (data) {
+         kordinatner.push(data);
+         io.sockets.emit("gciq",data);
    })
-   socket.on("dell", function (dell) {
-        messages=[]
-        sockets.emit("jnjel teg ", );
-       
-   })
+   
 });
